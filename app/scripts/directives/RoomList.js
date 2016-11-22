cb5b816a5ab364510e493c3c4e2cef2634f4fe67
Thuy Pham <tpham045@hotmail.com>
2016-11-22 23:49:23 +0000

    Call an uibModal when clicking on room name

--- app/scripts/directives/RoomList.js
+++ app/scripts/directives/RoomList.js
@@ -1,6 +1,6 @@
 (function() {
 
-  function RoomList(Room) {
+  function RoomList(Room, $uibModal, $document, $rootScope) {
     return {
       templateUrl: '/templates/directives/room-list.html',
       restrict: 'E',
@@ -10,9 +10,9 @@
         var roomArray = Room.all;
         scope.roomList = roomArray;
         /**
-        * @func updateRoom
-        * @desc 1) read user input 2) write to database 3)reset input field to default
-        */
+         * @func updateRoom
+         * @desc 1) read user input 2) write to database 3)reset input field to default
+         */
         scope.updateRoom = function(room) {
           if (scope.room) {
             scope.room = angular.copy(room);
@@ -21,21 +21,26 @@
           };
         };
         /**
-        * @func removeRoom
-        * @desc 1) remove a room using its index
-        */
+         * @func removeRoom
+         * @desc 1) remove a room using its index
+         */
         scope.removeRoom = function(room) {
-          Room.all.$remove(scope.roomList.indexOf(room));
-        }
-        /**
-        * @func openRoom
-        * @desc 1) open a chat space using its index
-        */
-        scope.openRoom = function() {
-          // display conversations
-        }
-        
-        
+            Room.all.$remove(scope.roomList.indexOf(room));
+          }
+          /**
+           * @func openRoom
+           * @desc 1) open a chat space using its index
+           */
+        scope.openRoom = function(room) {
+          $rootScope.room = angular.copy(room);
+          $uibModal.open({
+            appendTo: $document.find('body'),
+            ariaLabelledBy: 'modal-title',
+            ariaDescribedBy: 'modal-body',
+            templateUrl: '/templates/controllers/room-modal.html',
+          });
+        };
+
 
       }
     };
