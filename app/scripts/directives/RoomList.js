79b39631bda9433a6366b497f16c01296b357920
Thuy Pham <tpham045@hotmail.com>
2016-11-22 17:50:16 +0000

    Add room list

--- app/scripts/directives/RoomList.js
+++ app/scripts/directives/RoomList.js
@@ -1,32 +1,41 @@
 (function() {
 
-  function TaskSetting(Tasks) {
+  function RoomList(Room) {
     return {
       templateUrl: '/templates/directives/room-list.html',
       restrict: 'E',
       scope: {},
       link: function(scope, element, attributes) {
         //@var for tasks array
-        var taskArray = Tasks.all;
-        scope.taskList = taskArray;
+        var roomArray = Room.all;
+        scope.roomList = roomArray;
         /**
-        * @func updateTask
+        * @func updateRoom
         * @desc 1) read user input 2) write to database 3)reset input field to default
         */
-        scope.updateTask = function(task) {
-          if (scope.task) {
-            scope.task = angular.copy(task);
-            taskArray.$add(scope.task);
-            scope.task = '';
+        scope.updateRoom = function(room) {
+          if (scope.room) {
+            scope.room = angular.copy(room);
+            roomArray.$add(scope.room);
+            scope.room = '';
           };
         };
         /**
-        * @func removeTask
-        * @desc 1) remove a task using its index
+        * @func removeRoom
+        * @desc 1) remove a room using its index
         */
-        scope.removeTask = function(task) {
-          Tasks.all.$remove(scope.taskList.indexOf(task));
+        scope.removeRoom = function(room) {
+          Room.all.$remove(scope.roomList.indexOf(room));
         }
+        /**
+        * @func openRoom
+        * @desc 1) open a chat space using its index
+        */
+        scope.openRoom = function() {
+          // display conversations
+        }
+        
+        
 
       }
     };
@@ -34,5 +43,5 @@
 
   angular
     .module('blocChat')
-    .directive('roomList', TaskSetting);
+    .directive('roomList', RoomList);
 })();
---
+++ dist/scripts/services/Room.js
@@ -0,0 +1,16 @@
+(function() {
+  function Room($firebaseArray) {
+    var ref = firebase.database().ref().child("rooms");
+    // download room into a synchronized array
+    var rooms = $firebaseArray(ref);
+
+    return {
+      all: rooms
+        // remaining logic for room
+    };
+  }
+
+  angular
+    .module('blocChat')
+    .factory('Room', ['$firebaseArray', Room]);
+})();
